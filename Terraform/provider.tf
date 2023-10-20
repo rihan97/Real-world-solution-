f00a56d73bd36ead6465772d09173b1e77ec032a
rihan97 <rihan.alam97@gmail.com>
2023-10-20 17:01:28 +0100

    changing terraform version to 0.13

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,14 +1,3 @@
-# provider "aws" {
-#   aws = {
-#     version    = "~> 5.0"
-#     region     = "eu-west-1"
-#     access_key = "AKIAU7GNFBVE7LGMVN5L"
-#     secret_key = "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"
-#   }
-#   kubernetes = {
-#     version = ">= 2.10"
-#   }
-# }
 
 terraform {
   required_providers {
