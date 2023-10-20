b436c372fd03dae16fa3eea0c4cb758b16eb139e
Rayhan <rayhan.alam@digital.homeoffice.gov.uk>
2023-10-20 15:59:21 +0100

    created oidc provider and iam test role to test it out, output arn to the terminal for k8s sa account

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -305,3 +305,73 @@ resource "aws_eks_node_group" "private-nodes" {
     ]
 }
 
+
+# adding permissions to k8s either attach policy to nodes every pod has same perms to aws resources 
+# or create open id connect provider associate iam role with k8s service account this SA can then provide aws perms to containers in any pod which uses this sa
+
+# create cert for eks
+data "tls_certificate" "eks" {
+    url = aws_eks_cluster.dev.indentity[0].oidc[0].issuer
+}
+
+# create openid provider second option - iam roles
+resource "aws_iam_openid_connect_provider" "eks" {
+    client_id_list = ["sts.amazon.com"]
+    thumbprint_list = [data.tls_certificate.eks.certifcates[0].sha1_fingerprint]
+    url = aws_eks_cluster.dev.identity[0].oidc[0].issuer
+}
+
+
+# lets create a test iam role to assume the oidc role policy
+data "aws_iam_policy_document" "test_oidc_assume_role_policy" {
+    statement {
+        actions = ["sts:AssumeRoleWithWebIdentity"]
+        effect = "Allow"
+
+        condition {
+            test = "StringEquals"
+            variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+            values = ["system:serviceaccount:default:aws-test"] //create this sa aws-test in default ns in k8s
+        }
+
+        principals {
+            identifiers = [aws_iam_openid_connect_provider.eks.arn]
+            type = "Federated"
+        }
+    }
+}
+
+
+# create the iam role 
+resource "aws_iam_role" "test_oidc" {
+    assume_role_policy = data.aws_iam_policy_document.test_oidc_assume_role_policy.json
+    name = "test-oidc"
+}
+
+
+# now to test the created oidc provider lets give it s3 permission to list buckets
+resource "aws_iam_policy" "test_policy" {
+    name = "test-policy"
+
+    policy = jsonencode ({
+        Statement = [{
+            Action = [
+                "s3:ListAllMyBuckets",
+                "s3:GetBucketLocation"
+        ]
+        Effect = "Allow"
+        Resource = "arn:aws:s3:::*"
+    }]
+    Version = "2012-10-17"
+})
+}
+
+# attach the test policy to our role 
+resource "aws_iam_role_policy_attachment" "test_attach" {
+    role = aws_iam_role.test_oidc.name
+    policy_arn = aws_iam_policy.test-policy.arn
+}
+
+# output the arn of the created role to the terminal as its needed in the k8s sa - check outptus.tf
+
+
