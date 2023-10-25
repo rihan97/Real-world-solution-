8919a236afa1a979d6f54b74ad4646ba2d1240f4
rihan97 <rihan.alam97@gmail.com>
2023-10-25 10:36:10 +0100

    changed route block to objects instead of list

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -101,54 +101,26 @@ resource "aws_nat_gateway" "nat" {
 resource "aws_route_table" "private" {
     vpc_id = aws_vpc.main.id
 
-    route = [
-        {
+    route {
             cidr_block = "0.0.0.0/0" //default route to nat gw
             nat_gateway_id = aws_nat_gateway.nat.id
-            carrier_gateway_id = ""
-            destination_prefix_list_id = ""
-            egress_only_gateway_id = ""
-            gateway_id = ""
-            instance_id = ""
-            ipv6_cidr_block = ""
-            local_gateway_id = ""
-            network_interface_id = ""
-            transit_gateway_id = ""
-            vpc_endpoint_id = ""
-            vpc_peering_connection_id = ""
-            core_network_arn = ""
-
         }
-    ]
 
     tags = {
         name = "private"
     }
 }
 
+
+
 # public  rt with default route to igw
 resource "aws_route_table" "public" {
     vpc_id = aws_vpc.main.id
-
-    route = [
-        {
+    
+    route {
             cidr_block = "0.0.0.0/0" //default route to nat gw
             gateway_id = aws_internet_gateway.igw.id
-            nat_gateway_id = aws_nat_gateway.nat.id
-            carrier_gateway_id = ""
-            destination_prefix_list_id = ""
-            egress_only_gateway_id = ""
-            gateway_id = ""
-            instance_id = ""
-            ipv6_cidr_block = ""
-            local_gateway_id = ""
-            network_interface_id = ""
-            transit_gateway_id = ""
-            vpc_endpoint_id = ""
-            vpc_peering_connection_id = ""
-            core_network_arn = ""
         }
-    ]
 
     tags = {
         name = "public"
