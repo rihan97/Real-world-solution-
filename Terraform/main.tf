1937a3c0fe925db34ff8dbc6ae2056a2da5078a4
rihan97 <rihan.alam97@gmail.com>
2023-10-25 11:59:29 +0100

    eks cluster auto scaller role, policy and attachment created with output of arn to terminal

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -356,6 +356,7 @@ resource "aws_iam_role_policy_attachment" "test_attach" {
 
 
 
+
 # create eks autoscaler 
 # use previous created OpenID connect provider to create iam role and bind with the auto scaler
 
@@ -367,7 +368,51 @@ data "aws_iam_policy_document" "eks_cluster_autoscaler_assume_role_policy" {
     condition {
       test = "StringEquals"
       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values = ["system:"]
+      values = ["system:serviceaccount:kube-system:cluster-autoscaler"]  // the after scaler is being deployed in kube system ns different to previous ones
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+      type = "Federated"
     }
     }
 }
+
+# next the auto scaler eks cluster role 
+resource "aws_iam_role" "eks_cluster_autoscaler" {
+    assume_role_policy = data.aws_iam_policy_document.eks_cluster_autoscaler_assume_role_policy.json
+    name = "eks-cluster-autoscaler"
+}
+
+# the autoscaler policy
+resource "aws_iam_policy" "eks_cluster_autoscaler" {
+    name = "eks-cluster-autoscaler"
+
+    policy = jsonencode({
+        Statement = [{
+            Action = [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeLaunchConfigurations",
+                "autoscaling:DescribeTags",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup",
+                "ec2:DescribeLaunchTemplateVersions"
+            ]
+
+            Effect = "Allow"
+            Resource = "*"
+
+        }]
+        Version = "2012-10-17"
+    })
+  
+}
+
+# attach it to the same eks cluster role
+resource "aws_iam_policy_attachment" "eks_cluster_autoscaler_attach" {
+    role = aws_iam_role.eks_cluster_autoscaler.name
+    policy_policy_arn = aws_iam_policy.eks_cluster_autoscaler.arn 
+}
+
+# output the arn of role to the terminal check outputs file
--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -2,3 +2,7 @@
 output "test_policy_arn" {
     value = aws_iam_role.test_oidc.arn
 }
+
+output "eks_cluster_autoscaler-arn" {
+    value = aws_iam_role.eks_cluster_autoscaler.arn 
+}
